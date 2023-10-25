806969e8b55a11235772c6137eecc123ff6fac98
Tonny Silva <junior.silva.bernardino@gmail.com>
2023-10-25 11:32:58 -0300

    Fixing final errors in Components Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Hero from "./components/Hero";
 import About from "./components/About";
-import Bot from "./components/Bot";
+import Skills from "./components/Skills";
+import Project from "./components/Project";
+import Hireme from "./components/Hireme";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Hireme from "./components/Hireme";
 import Navbar from "./components/Navbar";
-import Project from "./components/Project";
-import Skills from "./components/Skills"
+import Bot from "./components/Bot";
 
 const App = () => {
   return (
@@ -24,3 +24,5 @@ const App = () => {
     </div>
   );
 };
+
+export default App;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,16 @@
 import React from "react"
 
 const Project = () => {
-    return <div>Project</div>
+  return <section id="projects" className="py-10 text-white">
+    <div className="text-center">
+      <h3 className="text-4xl font-semibold">
+        Hire <span className="text-cyan-600">Projects</span>
+      </h3>
+      <p className="text-gray-400 at-3 text-lg">Do you have any work?</p>
+    </div>
+  </section>
+  return (
+    <div>Return</div>
+  )
 };
-
 export default Project;

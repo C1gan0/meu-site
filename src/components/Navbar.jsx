e3eed52addaa736c9556297967a576040daf7b8e
Tonny Silva <junior.silva.bernardino@gmail.com>
2023-10-31 20:46:48 -0300

    creating the menu from navbar .jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav>
+  const menuLinks = [
+    { name: "HOME", link: "#home" },
+    { name: "ABOUT", link: "#about" },
+    { name: "SKILLS", link: "#skills" },
+    { name: "PROJECTS", link: "#projects" },
+    { name: "CONTACT", link: "#contact" },
+  ];
+  return  (
+  <nav>
 <div>
   <div className="mx-7">
     <h4 className="text-4xl uppercase font-bold">
-      C<span>BT</span>C
+      C<span className="text">BT</span>C
     </h4>
   </div>
+  <div>
+    <ul>
+      <li>
+        <a href=""></a>
+      </li>
+    </ul>
+  </div>
 </div>
-  </nav>;
+  </nav>
+  );
 };
 
 export default Navbar;
